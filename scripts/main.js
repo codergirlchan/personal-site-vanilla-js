e38cf37e4fd0e60edaa9cb219f587dd6886bc229
Chanelle Francis <codergirlchan@gmail.com>
2022-01-27 22:47:24 -0500

    finished nav and about section

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,19 @@
+// Toggle button
 const toggle = document.querySelector('#nav-toggle');
 const nav = document.querySelector('#header-nav');
 
-function navToggle() {
+function toggleNavClass() {
     nav.classList.toggle('nav-open');
 }
 
-toggle.onclick = navToggle;
+toggle.onclick = toggleNavClass;
+
+document.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+
+    if(scrollPosition >= 300) {
+        nav.classList.add('nav-solid');
+    } else {
+        nav.classList.remove('nav-solid');
+    }
+});
