b3ab967cc4985335509cf0dc8d181b3309c973aa
Chanelle Francis <codergirlchan@gmail.com>
2022-02-16 23:27:55 -0500

    adding nav slide

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,39 @@
 // Toggle button
 const toggle = document.querySelector('#nav-toggle');
 const nav = document.querySelector('#header-nav');
+const navLinks = document.querySelectorAll('.nav-link');
 
-function toggleNavClass() {
-    nav.classList.toggle('nav-open');
+function toggleNavClass(className) {
+    nav.classList.toggle(className);
 }
 
-toggle.onclick = toggleNavClass;
+toggle.onclick = function() {
+    toggleNavClass('nav-open');
+}
+
+if(window.screen.width < 768) {
+    navLinks.forEach(link => link.onclick = function() {
+        toggleNavClass('nav-open')
+    }); 
+}
+
+let scrollTimer; 
+
+function setScrollTimer() {
+    if(scrollTimer) {
+        clearTimeout(scrollTimer);   
+    }
+    scrollTimer = setTimeout(function() {
+          toggleNavClass('slideUp');
+    }, 150);
+}
 
 document.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
 
     if(scrollPosition >= 300) {
         nav.classList.add('nav-solid');
+        setScrollTimer();
     } else {
         nav.classList.remove('nav-solid');
     }
